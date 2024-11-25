379364dbd89671f5b14132ebbb361c559c4dc0d6
ytoku <ytoku@lplus.tokyo>
2024-11-26 04:43:06 +0900

    Move CLI code from lib.rs to main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,74 +16,38 @@ mod type_unsigned;
 mod test_parse_cbor_and_build_tree;
 
 use std::fmt::Write;
-use std::fs;
-use std::io::{self, Read};
-use std::path::PathBuf;
 
-use anyhow::Result;
-use clap::Parser;
+use anyhow::{bail, Result};
 
 use crate::cbor_object::ToTree;
 use crate::cbor_parser::parse_cbor;
-use crate::hex_parser::parse_hex;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short = 'H', long = "hex", default_value_t = false)]
-    hex: bool,
-
-    filepath: Option<PathBuf>,
+pub fn parse_hex(hex_content: &str) -> Result<Vec<u8>> {
+    let Ok((_, bytes_content)) = hex_parser::parse_hex(hex_content) else {
+        bail!("Error parsing hex data");
+    };
+    Ok(bytes_content)
 }
 
-pub fn run() -> Result<()> {
-    let args = Args::parse();
-
-    let bytes_content = if args.hex {
-        let hex_content = if let Some(filepath) = args.filepath {
-            fs::read_to_string(filepath)?
-        } else {
-            io::read_to_string(io::stdin())?
-        };
-
-        let Ok((rest, bytes_content)) = parse_hex(&hex_content) else {
-            eprintln!("Error parsing hex data");
-            std::process::exit(1);
-        };
-        if !rest.is_empty() {
-            eprintln!("Error parsing hex data: trailing bytes {:02x?}", rest);
-            std::process::exit(1);
-        }
-        bytes_content
-    } else {
-        if let Some(filepath) = args.filepath {
-            fs::read(filepath)?
-        } else {
-            let mut buffer = Vec::new();
-            io::stdin().read_to_end(&mut buffer)?;
-            buffer
-        }
-    };
-
-    let Ok((rest, object)) = parse_cbor(&bytes_content) else {
-        eprintln!("Error parsing CBOR data");
-        std::process::exit(1);
+pub fn dump_cbor_tree(bytes_content: &[u8]) -> Result<String> {
+    let Ok((rest, object)) = parse_cbor(bytes_content) else {
+        bail!("Error parsing CBOR data");
     };
 
     let cbor_tree = object.into_tree();
 
     let mut output = String::new();
     cbor_tree.write(&mut output);
-    print!("{}", output);
     if !rest.is_empty() {
-        println!(
+        writeln!(
+            &mut output,
             "trailing bytes {}",
             rest.iter().fold(String::new(), |mut acc, b| {
                 write!(acc, "{:02x}", b).unwrap();
                 acc
             })
-        );
+        )?;
     }
 
-    Ok(())
+    Ok(output)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,53 @@
+use std::fs;
+use std::io::{self, Read};
+use std::path::PathBuf;
+
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'H', long = "hex", default_value_t = false)]
+    hex: bool,
+
+    filepath: Option<PathBuf>,
+}
 
 fn main() -> Result<()> {
-    cbor_inspector::run()
+    let args = Args::parse();
+
+    #[allow(clippy::collapsible_else_if)]
+    let bytes_content = if args.hex {
+        let hex_content = if let Some(filepath) = args.filepath {
+            fs::read_to_string(filepath)?
+        } else {
+            io::read_to_string(io::stdin())?
+        };
+
+        match cbor_inspector::parse_hex(&hex_content) {
+            Ok(bytes_content) => bytes_content,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        if let Some(filepath) = args.filepath {
+            fs::read(filepath)?
+        } else {
+            let mut buffer = Vec::new();
+            io::stdin().read_to_end(&mut buffer)?;
+            buffer
+        }
+    };
+    let output = match cbor_inspector::dump_cbor_tree(&bytes_content) {
+        Ok(output) => output,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
+    print!("{}", output);
+    Ok(())
 }
